d5eab831f81f16987a6e4fb36f95da1a86153f6c
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-03-06 19:48:20 +0100

    Icons changes

--- FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
+++ FoodConsluting.Models/obj/Debug/net8.0/FoodConsluting.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d4539ffc746ada0de06bd8b88bb4108f5d18d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c5ec32ca6dbef16ad6fb0337b1c004ceda83ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodConsluting.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
